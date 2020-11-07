e8c23716986aefe174d44e6fdb8d2bd0dc0c2a78
Ramon Audain <raudain123@gmail.com>
2020-11-06 23:04:21 -0800

    Import Homepage

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import Homepage from "./components/Homepage";
 import './css/App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Homepage />
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+
